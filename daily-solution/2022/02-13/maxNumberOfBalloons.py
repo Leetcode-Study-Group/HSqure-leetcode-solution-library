5479664a6ac92499c7a45ec64703135be030b144
华桦 <hua.hua.hb5@is.naist.jp>
2022-02-13 21:51:17 +0800

    Create maxNumberOfBalloons.py

---
+++ daily-solution/2022/02-13/maxNumberOfBalloons.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        cnt = Counter(ch for ch in text if ch in "balon")
+        cnt['l']=cnt['l']//2
+        cnt['o']=cnt['o']//2
+        
+        return min(cnt.values()) if len(cnt)==5 else 0
